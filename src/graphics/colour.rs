c01a4e5d6e74a8eb82a1c58579da6e0b4d9bfb81
Siriusmart <sirius@siri.ws>
2025-12-05 16:28:07 +0000

    renamed Theme to Palette

--- src/graphics/colour.rs
+++ src/graphics/colour.rs
@@ -4,10 +4,10 @@ type ColourRGB = (u8, u8, u8);
 #[derive(Debug, Clone, Copy)]
 pub enum Colour {
     /// The primary colour of the gates (in a light theme, this would be black)
-    Foreground,
+    Fg,
 
     /// The background colour (in a light theme, this would be white)
-    Background,
+    Bg,
 
     /// Error colour (e.g. a FSM not enabled)
     Alert,
@@ -16,52 +16,51 @@ pub enum Colour {
     Info,
 
     /// Arbitrary RGB colour
-    RGB(u8, u8, u8)
+    RGB(u8, u8, u8),
 }
 
 /// Struct holding all the colour theming
-pub struct Theme {
-    foreground_colour: ColourRGB,
-    background_colour: ColourRGB,
-    alert_colour: ColourRGB,
-    info_colour: ColourRGB,
+pub struct Palette {
+    fg: ColourRGB,
+    bg: ColourRGB,
+    alert: ColourRGB,
+    info: ColourRGB,
 }
 
-impl Theme {
+impl Palette {
     pub fn light_theme() -> Self {
         Self {
-            foreground_colour: (0, 0, 0),
-            background_colour: (255, 255, 255),
-            alert_colour: (255, 0, 0),
-            info_colour: (0, 180, 250),
+            fg: (0, 0, 0),
+            bg: (255, 255, 255),
+            alert: (255, 0, 0),
+            info: (0, 180, 250),
         }
     }
 
     pub fn dark_theme() -> Self {
         Self {
-            foreground_colour: (255, 255, 255),
-            background_colour: (0, 0, 0),
-            alert_colour: (255, 0, 0),
-            info_colour: (0, 180, 250),
+            fg: (255, 255, 255),
+            bg: (0, 0, 0),
+            alert: (255, 0, 0),
+            info: (0, 180, 250),
         }
     }
 }
 
-impl Default for Theme {
+impl Default for Palette {
     fn default() -> Self {
         Self::light_theme()
     }
 }
 
-
 impl Colour {
-    pub fn to_rgb(&self, theme: &Theme) -> (u8, u8, u8) {
+    pub fn to_rgb(&self, theme: &Palette) -> (u8, u8, u8) {
         match self {
-            Colour::Foreground => theme.foreground_colour,
-            Colour::Background => theme.background_colour,
-            Colour::Alert => theme.alert_colour,
-            Colour::Info => theme.info_colour,
-            Colour::RGB(r, g, b) => (*r, *g, *b)
+            Colour::Fg => theme.fg,
+            Colour::Bg => theme.bg,
+            Colour::Alert => theme.alert,
+            Colour::Info => theme.info,
+            Colour::RGB(r, g, b) => (*r, *g, *b),
         }
     }
 }

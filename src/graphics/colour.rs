cfa3065449c5532aef10f12a723e827a204550cc
Siriusmart <sirius@siri.ws>
2025-12-05 16:49:16 +0000

    #22 removed logic, and add more colour variants

--- src/graphics/colour.rs
+++ src/graphics/colour.rs
@@ -1,5 +1,3 @@
-type ColourRGB = (u8, u8, u8);
-
 /// Enum representing the colour options available for gates
 #[derive(Debug, Clone, Copy)]
 pub enum Colour {
@@ -9,58 +7,46 @@ pub enum Colour {
     /// The background colour (in a light theme, this would be white)
     Bg,
 
-    /// Error colour (e.g. a FSM not enabled)
-    Alert,
+    /// Success colour (e.g. counter output in denary)
+    Success,
 
     /// Info colour (e.g. counter output in denary)
     Info,
 
+    /// Warn colour (e.g. potential incorrect operation)
+    Warn,
+
+    /// Error colour (e.g. a FSM not enabled)
+    Error,
+
+    Black,
+    Blue,
+    Cyan,
+    Green,
+    Grey,
+    Magenta,
+    Red,
+    White,
+    Yellow,
+
     /// Arbitrary RGB colour
-    RGB(u8, u8, u8),
-}
+    Rgb {
+        r: u8,
+        g: u8,
+        b: u8,
+    },
 
-/// Struct holding all the colour theming
-pub struct Palette {
-    fg: ColourRGB,
-    bg: ColourRGB,
-    alert: ColourRGB,
-    info: ColourRGB,
-}
+    /// Arbitrary RGBA colour
+    Rgba {
+        r: u8,
+        g: u8,
+        b: u8,
+        a: f32,
+    },
 
-impl Palette {
-    pub fn light_theme() -> Self {
-        Self {
-            fg: (0, 0, 0),
-            bg: (255, 255, 255),
-            alert: (255, 0, 0),
-            info: (0, 180, 250),
-        }
-    }
-
-    pub fn dark_theme() -> Self {
-        Self {
-            fg: (255, 255, 255),
-            bg: (0, 0, 0),
-            alert: (255, 0, 0),
-            info: (0, 180, 250),
-        }
-    }
-}
-
-impl Default for Palette {
-    fn default() -> Self {
-        Self::light_theme()
-    }
-}
-
-impl Colour {
-    pub fn to_rgb(&self, theme: &Palette) -> (u8, u8, u8) {
-        match self {
-            Colour::Fg => theme.fg,
-            Colour::Bg => theme.bg,
-            Colour::Alert => theme.alert,
-            Colour::Info => theme.info,
-            Colour::RGB(r, g, b) => (*r, *g, *b),
-        }
-    }
+    /// A named colour from a palette
+    Named {
+        palette: &'static str,
+        name: &'static str,
+    },
 }

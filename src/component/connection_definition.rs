7cde4ad07186d9a84ef881a2965e6dd7c7f2f569
Siriusmart <sirius@siri.ws>
2025-12-08 22:26:15 +0000

    changed identifier format to remove struct name

--- src/component/connection_definition.rs
+++ src/component/connection_definition.rs
@@ -4,9 +4,9 @@ pub struct ConnectionDefinition {
     pub version: u32,
 
     /// Data type the connection carries
-    pub data_type: (&'static str, &'static str, u16, u16), // e.g. (package_name, gate-name, semver major, semver minor)
+    pub data_type: (&'static str, u16, u16), // e.g. (package_name, semver major, semver minor)
 
     /// Connection identifier: the unique identifier for the connection type
     /// filled in by macro
-    pub identifier: (&'static str, &'static str, u16, u16), // e.g. (package_name, gate-name, semver major, semver minor)
+    pub identifier: (&'static str, u16, u16), // e.g. (package_name, semver major, semver minor)
 }

780b511ce1c256914b3e0eb5aaa8d8856686b2fa
Siriusmart <sirius@siri.ws>
2025-12-10 00:36:22 +0000

    added deserialize function to type trait

--- src/v0/component/data_type.rs
+++ src/v0/component/data_type.rs
@@ -1,4 +1,7 @@
 pub trait Type {
     // Serialize Type into bytes
     fn serialize(&self) -> Box<[u8]>;
+
+    // Deserialize Type from bytes
+    fn deserialize(bytes: Box<[u8]>) -> Self where Self: Sized;
 }
